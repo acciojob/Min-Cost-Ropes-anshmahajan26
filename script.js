951fef5d8735faa732e8077cb33bfaf5393f8859
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-11 07:29:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	  if (arr.length <= 1) return 0;
+
+  let cost = 0;
+
+  // Sort the array in ascending order
+  arr.sort((a, b) => a - b);
+
+  while (arr.length > 1) {
+    // Take two smallest ropes
+    let first = arr.shift();
+    let second = arr.shift();
+
+    let sum = first + second;
+    cost += sum;
+
+    // Insert sum back in sorted position
+    let i = 0;
+    while (i < arr.length && arr[i] < sum) {
+      i++;
+    }
+    arr.splice(i, 0, sum);
+  }
+
+  return cost;
   
 }
 
